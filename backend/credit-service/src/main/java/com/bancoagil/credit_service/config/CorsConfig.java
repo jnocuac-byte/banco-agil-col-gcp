e539ae512660eb8a6631b762521c1314eb579dea
Alejandro7337 <alejandroalba2605@gmail.com>
2025-10-27 20:58:46 -0500

    cambios para subir credit y account-service

--- backend/credit-service/src/main/java/com/bancoagil/credit_service/config/CorsConfig.java
+++ backend/credit-service/src/main/java/com/bancoagil/credit_service/config/CorsConfig.java
@@ -5,16 +5,15 @@ import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// Configuración CORS para permitir solicitudes desde el frontend
+// Configuración de CORS para permitir solicitudes desde el frontend
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
     
-    // Configurar CORS
+    // Permitir solicitudes CORS desde orígenes específicos
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
-        // Permitir solicitudes desde los orígenes del frontend
-        registry.addMapping("/api/**") // Ajusta el patrón según tus necesidades
-                .allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500", "http://localhost:3000") // Orígenes permitidos
+        registry.addMapping("/api/**") // Rutas que permiten CORS
+                .allowedOrigins("https://frontend-service-514751056677.us-central1.run.app") // Orígenes permitidos
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
                 .allowedHeaders("*") // Permitir todos los encabezados
                 .allowCredentials(true); // Permitir credenciales (cookies, autenticación)
